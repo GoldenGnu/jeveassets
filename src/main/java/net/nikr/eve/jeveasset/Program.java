4f75d77631e0445ca46cafbee3a09f51dd26b552
GoldenGnu <nkr@niklaskr.dk>
2009-08-18 12:04:50 +0000

    -Updated version number -Removed some tasks (Now added to the google project) -Removed task markers from the remaining tasks

--- src/main/java/net/nikr/eve/jeveasset/Program.java
+++ src/main/java/net/nikr/eve/jeveasset/Program.java
@@ -65,7 +65,7 @@ import org.apache.log4j.Logger;
 public class Program implements ActionListener {
 
 	//"Major.Minor.Bugfix [BETA n] [BUILD #n])";
-	public static final String PROGRAM_VERSION = "Release Candidate 8";
+	public static final String PROGRAM_VERSION = "1.0.0";
 	public static final String PROGRAM_NAME = "jEveAssets";
 	public static final int BUTTONS_HEIGHT = 22;
 	public static final int BUTTONS_WIDTH = 90;
@@ -102,174 +102,34 @@ public class Program implements ActionListener {
 	private EventList<EveAsset> eveAssetEventList;
 
 	public Program(){
-		//[DONE] Add setting for: ApiCharacterReader > characterListResponse.getCachedUntil();
-		//[DONE] Add setting for: ApiCorporationReader > corporationResponse.getCachedUntil()
-		//[DONE] Add setting for: ApiConquerableStationReader > stationResponse.getCachedUntil()
-		//[DONE] Cache the ConquerableStationList (AKA write/load xml)
-		//[DONE] AND/OR see CompositeMatcherEditor()
-		//[DONE] Add eve-central API (http://eve-central.com/home/develop.html)
-		//[DONE] Ship loadouts (with export to Eve-Online Xml / EFT)
-		//			You can import fittings in eve encoded with: US-ASCII, UTF-8, UTF-16
-		//[DONE] Splash screen
-		//[DONE] Catch uncaught exceptions
-		//[DONE] filters: contain, don't contain, equal, not equal
-		//[DONE] charactersNextUpdate (Settings) should be moved to account...
-		//[DONE] Add statusbar
-		//[DONE] Add wallet balance to Values
-		//[DONE] Refresh assets in thread
-		//[DONE] ApiManagerDialog > the table column, is a bad size
-		//[DONE] Save reordered columns
-		//[DONE] hide/show columns
-		//[DONE] Materials
-		//[DONE] TEST Corporation settings
-		//[DONE] Maintain columns width when adding/removing/moving column
-		//[DONE] Auto Rezising AKA MainPanel.updateColumnsWidth()
-		//[DONE] Set correct column width: MainPanel.updateColumnWidth(TableColumn column)
-		//[DONE] Streamline log output
-		//[DONE] convert XML to UTF-16
-		//[DONE] Improve Materials Dialog layout
-		//[DONE] TEST Corporation assets
-		//[DONE] Copy data from table
-		//[DONE] MainTanel jTable selection goes bullogs when filteringer
-		//[DONE] Make flag to enable/disable jTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-		//[DONE] Total value of shown and of selected assets
-		//[DONE] Improve loadouts dialog layout
-		//[DONE] Improve values dialog layout
-		//[DONE] obey CacheUntil, when update failes... (All Eve-API readers)
-		//[DONE] TEST load/save with DEBUG = false
-		//[DONE] New columns: Highest buy price and Lowest sell price
-		//[DONE] T3 Ships flags
-		//[DONE] Remove the trailing "isk" from price columns
-		//[DONE] Values dialog, close button hidden...
-		//[DONE] make Materials dialog resizable
-		//[DONE] When updaing assets, show what time the assets can be updated again
-		//[DONE] Table pop-up: Add filter > Contain, Don't contain, Equals, Don't Equals
-		//[DONE] Columns changes, aka change coulmns all places needed
-		//[DONE] CSV export
-		//[DONE] The filter drop-down options should be: Equals, Does not equal, Contains, Does not contain
-		//[DONE] Add a thousands separator in the count column
-		//[DONE] Add price column, and totals to loadout dialog
-		//[DONE] Above & Below filter
-		//[DONE] The owner was set incorrect at times, should be fixed now (not confirmed)
-		//[DONE] Tried again to fix the Location bug
-		//[DONE] New asset and price update mechanism
-		//[DONE] Faster startup and more up-to-date assets results
-		//[DONE] Added options for EVE-Central prices
-		//[DONE] Fixed program crashing, if not connected to the internet...
-		//[DONE] Fixed update problem
-		//[DONE] Replace Collection with List
-		//[DONE] Equals and !Equals for number columns doesn't work as intented
-		//[DONE] Added more info to the statusbar
-		//[DONE] Manually set price for assets
-		//[DONE] Change the way to loop through map
-		//BETA 11:
-		//[DONE] Added market hubs: Rens, Oursalert, Amarr, Hek
-		//[DONE] Updated the save filter dialog
-		//[DONE] Set Price: Focus JTextField on setVisable()
-		//[DONE] Eve-Central Options: Add all empire regions one by one, and Remove Eve Galaxy
-		//[DONE] Show character assets on/of
-		//[DONE] TypeID column
-		//[DONE] Volume column
-		//BETA 12:
-		//[DONE] Region column
-		//[DONE] AssetFilter column will be eraised (now use name instead of index)
-		//[DONE] Focus on show...
-		//Release Candidate 1:
-		//[DONE] Fixed values grand total: total and wallet balance
-		//[DONE] Fixed bug with filter mode combobox
-		//[DONE] Added popup-menu with copy and paste to all text components
-		//[DONE] Price column: Now have gray background if the price was set manually
-		//[DONE] Meta column: Fixed a display issue with trailing zero...
-		//[DONE] Now search clipboard for API user id & key, when adding API Key
-		//[DONE] Started code clean-up
-		//Release Candidate 1a:
-		//[DONE] Fixed bug in Save Filter Dialog
-		//Release Candidate 1b:
-		//[DONE] New column: Type Count (Total count of this type of asset)
-		//[DONE] Apocrypha 1.3.1 data update...
-		//Release Candidate 2:
-		//[DONE] fixed bug that would make some dialogs to small
-		//Release Candidate 3:
-		//[DONE] fixed bug that would make some buttons to small
-		//[DONE] CSV Export: Added more options
-		//[DONE] mark blueprints as BPO/BPC
-		//[DONE] Added option to only filter when enter is pressed
-		//Release Candidate 4:
-		//[DONE] Corrected contact information
-		//[DONE] Fixed hidden characters being shown in the values tool
-		//[DONE] Ship Loadouts now show all ships
-		//Release Candidate 5:
-		//[DONE] Java 5 compile (with bad luck)
-		//Release Candidate 6:
-		//[DONE] Compiled with Java 6, again...
-		//[DONE] It's now possible to mark multiple items as BPOs
-		//[DONE] Better exit progress
-		//Release Candidate 7:
-		//[DONE] Ship Loadouts: Fixed bug that showed charges instead of modules
-		//[DONE] Fix bug that prevented the price data from being updated
-		//[DONE] Updated eveapi library to version 1.0.0
-		//[DONE] Updated nikr log library
-		//Release Candidate 7a:
-		//[DONE] Fixed price data bug, introduced in RC7
+		//FIXME before release: Fix copyright in all source files before release
+		//FIXME before release: Update version number and splash screen
+		//FIXME before release: set DEBUG, FORCE_UPDATE, and FORCE_NO_UPDATE to false (and clean build)
+		//FIXME before release: Write change log
 
-		//[FIXME] before release: Fix copyright in all source files before release
-		//[FIXME] before release: Update version number and splash screen
-		//[FIXME] before release: set DEBUG, FORCE_UPDATE, and FORCE_NO_UPDATE to false (and clean build)
-		//[FIXME] before release: Write change log
-
-		//[PENDING] TEST converted locationIDs:
-		//				Items in space AKA secure container (should work)
-		//				POS (should work)
-		//				NPC Stations (should work)
-		//				Offices (should work)
-
-		//[TODO] Reprocessing Value
-
-		//[TODO] Industry Jobs (personal and corporate)
-		//			Need for the complete list of assets
-
-		//[TODO] Market Orders (personal and corporate)
-		//			Need for the complete list of assets
+		//TEST converted locationIDs:
+		//		Items in space AKA secure container (should work)
+		//		POS (should work)
+		//		NPC Stations (should work)
+		//		Offices (should work)
 		
-		//[TODO] Edit account
+		//Enter saves:
+		//		+Set Price
+		//		-Save Filter
+		//		-Add API Key
+		//		-Export CSV
 
-		//[TODO] Print function
+		//Item browser
 		
-		//[TODO] Enter saves:
-		//			+Set Price
-		//			-Save Filter
-		//			-Add API Key
-		//			-Export CSV
+		//Auto Complete for FilterPanel
+
+		//Wallet Journal
+
+		//Wallet Transactions
 		
-		//[TODO] Clean up functions in dialogs and panels
-		//			-Check private/public
-		//				-Also check naming, could it be better?
+		//Custom price report, with price data from multiple regions
 
-		//[TODO] Item browser
-
-		//[TODO] Proxy Server
-
-		//[TODO] Cargo calc (how much of asset type can be in (user set) cargo hold)
-		//			Right click table to show (Only when one is selected)
-		
-		//[TODO] Auto Complete for FilterPanel
-
-		//[TODO] Delete row(s) of asset
-
-		//[TODO] CSV export tools export
-
-		//[TODO] POS Fuel
-		//			One click export
-
-		//[TODO] Wallet Journal
-
-		//[TODO] Wallet Transactions
-
-		//[TODO] Write javadoc...
-		
-		//[TODO] Custom price report, with price data from multiple regions
-
-		/*//[TODO] Category report
+		/*Category report
 					1. Sorts as per the main page. In my case that would allow me to run
 					   reports for various combinations of characters. Eve Assets does it by
 					   allowing you to choose saved filters, from memory.
@@ -279,11 +139,8 @@ public class Program implements ActionListener {
 					4. A csv export function
 		 */
 
-		//[PENDING] Program might crash if computer is disconnected while updating
-		//			Hard to test, ehh...
-
-		//[PENDING] it seems when i activate a filter and afterwards add another char/api
-		//		 it throws an unhandled exception when updating
+		//it seems when i activate a filter and afterwards add another char/api
+		//		it throws an unhandled exception when updating
 		//			Can't reproduce...
 		
 		Log.info("Starting "+PROGRAM_NAME+" "+PROGRAM_VERSION);
@@ -337,7 +194,7 @@ public class Program implements ActionListener {
 		Log.info("	Eve-Central update Dialog");
 		updateEveCentralDialog = new UpdateEveCentralDialog(this, frame);
 		Log.info("	Proxy Settings Dialog");
-		proxySettingsDialogue = new ProxySettingsDialogue(this, ImageGetter.getImage("server_connect.png")); // TODO find another image.
+		proxySettingsDialogue = new ProxySettingsDialogue(this, ImageGetter.getImage("server_connect.png"));
 		Log.info("	GUI loaded");
 		SplashUpdater.setProgress(90);
 		Log.info("Updating data...");
