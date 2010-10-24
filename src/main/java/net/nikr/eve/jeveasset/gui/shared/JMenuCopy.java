ba6addecaa0147756b813b81509a4f50b058fe3d
GoldenGnu <nkr@niklaskr.dk>
2010-10-24 17:27:38 +0000

    Bug fix to Table Menu Copy

--- src/main/java/net/nikr/eve/jeveasset/gui/shared/JMenuCopy.java
+++ src/main/java/net/nikr/eve/jeveasset/gui/shared/JMenuCopy.java
@@ -44,6 +44,7 @@ public class JMenuCopy extends JMenuItem implements ActionListener, ClipboardOwn
 
 	public JMenuCopy(JTable jTable) {
 		super("Copy");
+		this.jTable = jTable;
 		this.setIcon(Images.ICON_COPY);
 		this.setActionCommand(ACTION_COPY);
 		this.addActionListener(this);
