8866cde7ad4e5cd4895e162bf36d53b5cafc8284
GoldenGnu <nkr@niklaskr.dk>
2010-11-10 14:01:53 +0000

    Fixed copyright statements in source files

--- src/main/java/net/nikr/eve/jeveasset/io/shared/DesktopUtil.java
+++ src/main/java/net/nikr/eve/jeveasset/io/shared/DesktopUtil.java
@@ -1,6 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2009, 2010 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  */
 
 package net.nikr.eve.jeveasset.io.shared;
@@ -15,10 +31,7 @@ import net.nikr.eve.jeveasset.Program;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- *
- * @author Niklas
- */
+
 public class DesktopUtil {
 
 	private final static Logger LOG = LoggerFactory.getLogger(DesktopUtil.class);
