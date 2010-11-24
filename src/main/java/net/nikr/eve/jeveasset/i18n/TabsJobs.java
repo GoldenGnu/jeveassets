21ec9bbfbe49347d5c7ce60c581a8bcc52b86d91
Flaming Candle <na@na.com>
2010-11-24 23:22:05 +0000

    i18ned the gui/tabs/jobs package. - added the missing files.

---
+++ src/main/java/net/nikr/eve/jeveasset/i18n/TabsJobs.java
@@ -0,0 +1,28 @@
+package net.nikr.eve.jeveasset.i18n;
+
+import java.util.Locale;
+import uk.me.candle.translations.Bundle;
+import uk.me.candle.translations.BundleCache;
+
+public abstract class TabsJobs extends Bundle {
+	public static TabsJobs get() {
+		return BundleCache.get(TabsJobs.class);
+	}
+
+	public static TabsJobs get(Locale locale) {
+		return BundleCache.get(TabsJobs.class, locale);
+	}
+
+	public TabsJobs(Locale locale) {
+		super(locale);
+	}
+
+	public abstract String activity();
+	public abstract String character();
+	public abstract String all();
+	public abstract String industry();
+	public abstract String install();
+	public abstract String no();
+	public abstract String state();
+	public abstract String whitespace(Object arg0);
+}
