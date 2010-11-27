2b605b8991f1aaa5768ac73364de3d55f86a6d0d
Flaming Candle <na@na.com>
2010-11-27 14:02:44 +0000

    i18ned the gui/tabs/orders package. - added missing files.

---
+++ src/main/java/net/nikr/eve/jeveasset/i18n/TabsOrders.java
@@ -0,0 +1,29 @@
+package net.nikr.eve.jeveasset.i18n;
+
+import java.util.Locale;
+import uk.me.candle.translations.Bundle;
+import uk.me.candle.translations.BundleCache;
+
+public abstract class TabsOrders extends Bundle {
+	public static TabsOrders get() {
+		return BundleCache.get(TabsOrders.class);
+	}
+
+	public static TabsOrders get(Locale locale) {
+		return BundleCache.get(TabsOrders.class, locale);
+	}
+
+	public TabsOrders(Locale locale) {
+		super(locale);
+	}
+
+	public abstract String buy();
+	public abstract String buy1();
+	public abstract String character();
+	public abstract String market();
+	public abstract String no();
+	public abstract String sell();
+	public abstract String sell1();
+	public abstract String state();
+	public abstract String whitespace(Object arg0);
+}
