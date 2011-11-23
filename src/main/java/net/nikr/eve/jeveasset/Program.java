54516e429e50e4362eb0c33c7e39f6e4ff758db1
GoldenGnu <nkr@niklaskr.dk>
2011-11-23 12:43:47 +0000

    Making ready to release 1.9.1

--- src/main/java/net/nikr/eve/jeveasset/Program.java
+++ src/main/java/net/nikr/eve/jeveasset/Program.java
@@ -76,7 +76,7 @@ public class Program implements ActionListener, Listener<Asset>{
 	private final static Logger LOG = LoggerFactory.getLogger(Program.class);
 
 	//Major.Minor.Bugfix [Release Candidate n] [BETA n] [DEV BUILD #n];
-	public static final String PROGRAM_VERSION = "1.9.0";
+	public static final String PROGRAM_VERSION = "1.9.1";
 	public static final String PROGRAM_NAME = "jEveAssets";
 	public static final String PROGRAM_UPDATE_URL = "http://eve.nikr.net/jeveassets/update.xml";
 	public static final String PROGRAM_HOMEPAGE = "http://eve.nikr.net/jeveasset";
