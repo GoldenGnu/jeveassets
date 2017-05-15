ef048cdde26f892abe6cde094c26eb7f8a96e1b5
GoldenGnu <GoldenGnu@users.noreply.github.com>
2017-05-15 14:36:03 +0200

    Code: Ignore disabled ESI Scopes in update

--- src/main/java/net/nikr/eve/jeveasset/gui/dialogs/update/UpdateDialog.java
+++ src/main/java/net/nikr/eve/jeveasset/gui/dialogs/update/UpdateDialog.java
@@ -51,6 +51,7 @@ import net.nikr.eve.jeveasset.io.esi.EsiAccountBalanceGetter;
 import net.nikr.eve.jeveasset.io.esi.EsiAssetsGetter;
 import net.nikr.eve.jeveasset.io.esi.EsiOwnerGetter;
 import net.nikr.eve.jeveasset.io.esi.EsiStructuresGetter;
+import net.nikr.eve.jeveasset.io.esi.Scopes;
 import net.nikr.eve.jeveasset.io.eveapi.AccountBalanceGetter;
 import net.nikr.eve.jeveasset.io.eveapi.AccountGetter;
 import net.nikr.eve.jeveasset.io.eveapi.AssetsGetter;
@@ -714,8 +715,10 @@ public class UpdateDialog extends JDialogCentered {
 			if (!program.getProfileManager().getEsiOwners().isEmpty()) {
 				setIcon(Images.MISC_ESI.getIcon());
 			}
-			EsiAssetsGetter esiAssetsGetter = new EsiAssetsGetter();
-			esiAssetsGetter.load(this, program.getProfileManager().getEsiOwners());
+			if (Scopes.ASSETS.isEnabled()) {
+				EsiAssetsGetter esiAssetsGetter = new EsiAssetsGetter();
+				esiAssetsGetter.load(this, program.getProfileManager().getEsiOwners());
+			}
 		}
 	}
 
@@ -743,8 +746,10 @@ public class UpdateDialog extends JDialogCentered {
 			if (!program.getProfileManager().getEsiOwners().isEmpty()) {
 				setIcon(Images.MISC_ESI.getIcon());
 			}
-			EsiAccountBalanceGetter esiAccountBalanceGetter = new EsiAccountBalanceGetter();
-			esiAccountBalanceGetter.load(this, program.getProfileManager().getEsiOwners());
+			if (Scopes.ACCOUNT_BALANCE.isEnabled()) {
+				EsiAccountBalanceGetter esiAccountBalanceGetter = new EsiAccountBalanceGetter();
+				esiAccountBalanceGetter.load(this, program.getProfileManager().getEsiOwners());
+			}
 		}
 	}
 
