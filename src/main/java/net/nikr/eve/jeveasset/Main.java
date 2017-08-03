0719f94678fd1d22038036d83b47ff28231a8a6b
GoldenGnu <GoldenGnu@users.noreply.github.com>
2017-08-03 14:28:20 +0200

    Code: Updated pricing library to 1.6.1 (Fixing Eve-Marketdata support)

--- src/main/java/net/nikr/eve/jeveasset/Main.java
+++ src/main/java/net/nikr/eve/jeveasset/Main.java
@@ -20,9 +20,6 @@
  */
 package net.nikr.eve.jeveasset;
 
-import static net.nikr.eve.jeveasset.Program.PROGRAM_NAME;
-import static net.nikr.eve.jeveasset.Program.PROGRAM_VERSION;
-
 import java.io.File;
 import java.net.URISyntaxException;
 import java.util.HashSet;
@@ -58,7 +55,7 @@ public final class Main {
 		SplashUpdater splashUpdater = new SplashUpdater();
 		splashUpdater.start();
 		//Print program data
-		log.info("Starting " + PROGRAM_NAME + " " + PROGRAM_VERSION);
+		log.info("Starting " + Program.PROGRAM_NAME + " " + Program.PROGRAM_VERSION);
 		log.info("OS: " + System.getProperty("os.name") + " " + System.getProperty("os.version"));
 		log.info("Java: " + System.getProperty("java.vendor") + " " + System.getProperty("java.version"));
 		// variables to the main program and settings.
@@ -233,7 +230,7 @@ public final class Main {
 		files.add("jcommon-1.0.16.jar");
 		files.add("jfreechart-1.0.13.jar");
 		files.add("osxadapter-1.1.0.jar");
-		files.add("pricing-1.6.0.jar");
+		files.add("pricing-1.6.1.jar");
 		files.add("routing-1.5.0.jar");
 		files.add("slf4j-api-1.7.25.jar");
 		files.add("log4j-over-slf4j-1.7.25.jar");
