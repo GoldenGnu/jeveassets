365a86a31b008307ed8cb060564015bd4389587b
GoldenGnu <GoldenGnu@users.noreply.github.com>
2017-10-13 20:04:35 +0200

    Release: Making ready to release 4.5.1

--- src/main/java/net/nikr/eve/jeveasset/Program.java
+++ src/main/java/net/nikr/eve/jeveasset/Program.java
@@ -94,7 +94,7 @@ public class Program implements ActionListener {
 		TIMER
 	}
 	//Major.Minor.Bugfix [Release Candidate n] [BETA n] [DEV BUILD #n];
-	public static final String PROGRAM_VERSION = "4.5.1 DEV BUILD 1";
+	public static final String PROGRAM_VERSION = "4.5.1";
 	public static final String PROGRAM_NAME = "jEveAssets";
 	public static final String PROGRAM_HOMEPAGE = "https://eve.nikr.net/jeveasset";
 	public static final boolean PROGRAM_FORCE_PORTABLE = false;
