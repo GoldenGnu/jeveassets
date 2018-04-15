89e67e8c985ed1abc7c71c8466b62ab8ff9801b8
GoldenGnu <GoldenGnu@users.noreply.github.com>
2018-04-15 09:34:55 +0200

    Code: Fixed build failure

--- src/main/java/net/nikr/eve/jeveasset/Main.java
+++ src/main/java/net/nikr/eve/jeveasset/Main.java
@@ -227,7 +227,7 @@ public final class Main {
 		Set<String> files = new HashSet<String>();
 		files.add("asm-5.0.4.jar");
 		files.add("dom4j-2.1.0.jar");
-		files.add("eveapi-7.0.4-SNAPSHOT.jar");
+		files.add("eveapi-7.0.3.jar");
 		files.add("glazedlists-1.11.0.jar");
 		files.add("graph-1.5.0.jar");
 		files.add("guava-r09.jar");
