3e81a95145919240c3c554d8b246e0a68c50fbd3
GoldenGnu <GoldenGnu@users.noreply.github.com>
2019-11-30 20:31:48 +0100

    Code: Switched from GET/characters/{character_id}/ to POST/characters/affiliation/ and from GET/corporations/{corporation_id}/ to POST/universe/names/

--- src/main/java/net/nikr/eve/jeveasset/io/esi/AbstractEsiGetter.java
+++ src/main/java/net/nikr/eve/jeveasset/io/esi/AbstractEsiGetter.java
@@ -69,7 +69,6 @@ public abstract class AbstractEsiGetter extends AbstractGetter<EsiOwner> {
 	private static final ApiClient PUBLIC_CLIENT = new ApiClientBuilder().okHttpClient(getHttpClient()).build();
 	private static final UniverseApi UNIVERSE_API = new UniverseApi(PUBLIC_CLIENT);
 	private static final CharacterApi CHARACTER_API = new CharacterApi(PUBLIC_CLIENT);
-	private static final CorporationApi CORPORATION_API = new CorporationApi(PUBLIC_CLIENT);
 	private static final SovereigntyApi SOVEREIGNTY_API = new SovereigntyApi(PUBLIC_CLIENT);
 	private static final MarketApi MARKET_API = new MarketApi(PUBLIC_CLIENT);
 	public static final UserInterfaceApi USER_INTERFACE_API = new UserInterfaceApi(PUBLIC_CLIENT);
@@ -294,10 +293,6 @@ public abstract class AbstractEsiGetter extends AbstractGetter<EsiOwner> {
  
 	public CharacterApi getCharacterApiOpen() {
 		return CHARACTER_API;
- 	}
- 
-	public CorporationApi getCorporationApiOpen() {
-		return CORPORATION_API;
  	}
  
 	public SovereigntyApi getSovereigntyApiOpen() {
