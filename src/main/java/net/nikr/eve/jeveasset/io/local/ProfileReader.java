180ff5ec381f46a5627360178c7666836ce0644c
GoldenGnu <GoldenGnu@users.noreply.github.com>
2020-08-01 02:25:57 +0200

    Feature Fix: Fixed loading of skills

--- src/main/java/net/nikr/eve/jeveasset/io/local/ProfileReader.java
+++ src/main/java/net/nikr/eve/jeveasset/io/local/ProfileReader.java
@@ -890,6 +890,7 @@ public final class ProfileReader extends AbstractXmlReader<Boolean> {
 				skill.setSkillpoints(skillpoints);
 				skill.setActiveSkillLevel(activeSkillLevel);
 				skill.setTrainedSkillLevel(trainedSkillLevel);
+				skills.add(skill);
 			}
 			owners.setSkills(skills);
 			owners.setTotalSkillPoints(totalSkillPoints);
