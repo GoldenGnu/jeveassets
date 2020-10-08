915360b6559213aba7b4a14a3a951c3e920bf294
GoldenGnu <GoldenGnu@users.noreply.github.com>
2020-10-08 23:45:28 +0200

    Code: Updated eve-esi to 4.1.0

--- src/test/java/net/nikr/eve/jeveasset/io/local/ItemFlagsTest.java
+++ src/test/java/net/nikr/eve/jeveasset/io/local/ItemFlagsTest.java
@@ -29,6 +29,6 @@ public class ItemFlagsTest extends TestUtil {
 
 	@Test
 	public void testItemFlags() {
-		assertEquals(133, StaticData.get().getItemFlags().size());
+		assertEquals(134, StaticData.get().getItemFlags().size());
 	}
 }
