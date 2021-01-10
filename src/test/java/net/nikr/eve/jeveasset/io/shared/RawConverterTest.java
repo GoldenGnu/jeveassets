df8f1d95fd45aa00219dc39fa8243a4c60d855bc
GoldenGnu <GoldenGnu@users.noreply.github.com>
2021-01-10 18:24:19 +0100

    Code: Updated eve-esi to 4.2.0

--- src/test/java/net/nikr/eve/jeveasset/io/shared/RawConverterTest.java
+++ src/test/java/net/nikr/eve/jeveasset/io/shared/RawConverterTest.java
@@ -334,7 +334,7 @@ public class RawConverterTest extends TestUtil {
 
 	@Test
 	public void testToJournalRefType_CharacterWalletJournalResponseRefTypeEnum() {
-		assertEquals(120, CharacterWalletJournalResponse.RefTypeEnum.values().length);
+		assertEquals(121, CharacterWalletJournalResponse.RefTypeEnum.values().length);
 		int undefined = 0;
 		for (CharacterWalletJournalResponse.RefTypeEnum refType : CharacterWalletJournalResponse.RefTypeEnum.values()) {
 			RawJournalRefType rawJournalRefType = RawConverter.toJournalRefType(refType);
@@ -348,7 +348,7 @@ public class RawConverterTest extends TestUtil {
 
 	@Test
 	public void testToJournalRefType_CorporationWalletJournalResponseRefTypeEnum() {
-		assertEquals(118, CorporationWalletJournalResponse.RefTypeEnum.values().length);
+		assertEquals(121, CorporationWalletJournalResponse.RefTypeEnum.values().length);
 		int undefined = 0;
 		for (CorporationWalletJournalResponse.RefTypeEnum refType : CorporationWalletJournalResponse.RefTypeEnum.values()) {
 			RawJournalRefType rawJournalRefType = RawConverter.toJournalRefType(refType);
