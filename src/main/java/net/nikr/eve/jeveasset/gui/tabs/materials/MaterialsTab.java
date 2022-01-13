c9c9db8c7965a7fd1f58b0b66ead30201c0e033d
GoldenGnu <GoldenGnu@users.noreply.github.com>
2022-01-13 22:31:48 +0100

    Code: Fixed class name

--- src/main/java/net/nikr/eve/jeveasset/gui/tabs/materials/MaterialsTab.java
+++ src/main/java/net/nikr/eve/jeveasset/gui/tabs/materials/MaterialsTab.java
@@ -166,7 +166,7 @@ public class MaterialsTab extends JMainTabSecondary {
 		installTableTool(new MaterialTableMenu(), tableFormat, tableModel, jTable, eventList, Material.class);
 
 		List<EnumTableColumn<Material>> enumColumns = new ArrayList<>();
-		enumColumns.addAll(Arrays.asList(MaterialExtenedTableFormat.values()));
+		enumColumns.addAll(Arrays.asList(MateriaExtendedTableFormat.values()));
 		enumColumns.addAll(Arrays.asList(MaterialTableFormat.values()));
 		exportDialog = new ExportDialog<>(program.getMainWindow().getFrame(), NAME, null, new MaterialsFilterControl(), Collections.singletonList(eventList), enumColumns);
 
@@ -390,7 +390,7 @@ public class MaterialsTab extends JMainTabSecondary {
 		@Override
 		protected Object getColumnValue(final Material item, final String column) {
 			try {
-				return MaterialExtenedTableFormat.valueOf(column).getColumnValue(item);
+				return MateriaExtendedTableFormat.valueOf(column).getColumnValue(item);
 			} catch (IllegalArgumentException exception) {
 
 			}
@@ -400,7 +400,7 @@ public class MaterialsTab extends JMainTabSecondary {
 		@Override
 		protected EnumTableColumn<Material> valueOf(final String column) {
 			try {
-				return MaterialExtenedTableFormat.valueOf(column);
+				return MateriaExtendedTableFormat.valueOf(column);
 			} catch (IllegalArgumentException exception) {
 
 			}
