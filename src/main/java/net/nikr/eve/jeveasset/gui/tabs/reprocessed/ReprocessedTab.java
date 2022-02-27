73fd0cd8f4324f50ae3aa080a2954a39e193cf5d
GoldenGnu <nkr@niklaskr.dk>
2022-02-27 13:55:32 +0100

    Added Copyright to new file and fixed imports

--- src/main/java/net/nikr/eve/jeveasset/gui/tabs/reprocessed/ReprocessedTab.java
+++ src/main/java/net/nikr/eve/jeveasset/gui/tabs/reprocessed/ReprocessedTab.java
@@ -38,7 +38,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
