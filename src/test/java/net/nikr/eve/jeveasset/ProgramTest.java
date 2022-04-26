7707f490b2143c4f27a6d4c66e4025966bd1f06c
GoldenGnu <GoldenGnu@users.noreply.github.com>
2022-04-26 17:22:09 +0200

    Code: Added test to ensure dev build is off (false)

---
+++ src/test/java/net/nikr/eve/jeveasset/ProgramTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2009-2022 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+package net.nikr.eve.jeveasset;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class ProgramTest extends TestUtil {
+
+	@Test
+	public void isDevBuild() {
+		assertFalse("Program.PROGRAM_DEV_BUILD = true (not allowed to be committed)", Program.isDevBuild());
+	}
+
+}
