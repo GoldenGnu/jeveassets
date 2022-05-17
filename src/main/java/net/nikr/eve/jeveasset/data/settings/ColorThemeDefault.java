8e67bbaadb2a068640f1a121b132fc8cca15665e
GoldenGnu <GoldenGnu@users.noreply.github.com>
2022-05-17 12:47:34 +0200

    New Feature: Added configurable colors to the industry jobs activity column (Issue #324)

--- src/main/java/net/nikr/eve/jeveasset/data/settings/ColorThemeDefault.java
+++ src/main/java/net/nikr/eve/jeveasset/data/settings/ColorThemeDefault.java
@@ -67,6 +67,15 @@ public class ColorThemeDefault extends ColorTheme {
 		colors.put(ColorEntry.MARKET_ORDERS_NEW, new ColorThemeEntry(Colors.LIGHT_GREEN));
 		colors.put(ColorEntry.INDUSTRY_JOBS_DELIVERED, new ColorThemeEntry(Colors.LIGHT_YELLOW));
 		colors.put(ColorEntry.INDUSTRY_JOBS_DONE, new ColorThemeEntry(Colors.LIGHT_GREEN));
+		colors.put(ColorEntry.INDUSTRY_JOBS_ACTIVITY_MANUFACTURING, new ColorThemeEntry(Colors.LIGHT_ORANGE));
+		colors.put(ColorEntry.INDUSTRY_JOBS_ACTIVITY_RESEARCHING_TECHNOLOGY, new ColorThemeEntry(Colors.LIGHT_BLUE));
+		colors.put(ColorEntry.INDUSTRY_JOBS_ACTIVITY_RESEARCHING_METERIAL_PRODUCTIVITY, new ColorThemeEntry(Colors.LIGHT_BLUE));
+		colors.put(ColorEntry.INDUSTRY_JOBS_ACTIVITY_RESEARCHING_TIME_PRODUCTIVITY, new ColorThemeEntry(Colors.LIGHT_BLUE));
+		colors.put(ColorEntry.INDUSTRY_JOBS_ACTIVITY_COPYING, new ColorThemeEntry(Colors.LIGHT_BLUE));
+		colors.put(ColorEntry.INDUSTRY_JOBS_ACTIVITY_DUPLICATING, new ColorThemeEntry(Colors.LIGHT_BLUE));
+		colors.put(ColorEntry.INDUSTRY_JOBS_ACTIVITY_REVERSE_ENGINEERING, new ColorThemeEntry(Colors.LIGHT_BLUE));
+		colors.put(ColorEntry.INDUSTRY_JOBS_ACTIVITY_REVERSE_INVENTION, new ColorThemeEntry(Colors.LIGHT_BLUE));
+		colors.put(ColorEntry.INDUSTRY_JOBS_ACTIVITY_REACTIONS, new ColorThemeEntry(Colors.LIGHT_GREEN));
 		colors.put(ColorEntry.INDUSTRY_SLOTS_FREE, new ColorThemeEntry(Colors.LIGHT_GREEN));
 		colors.put(ColorEntry.INDUSTRY_SLOTS_DONE, new ColorThemeEntry(Colors.LIGHT_YELLOW));
 		colors.put(ColorEntry.INDUSTRY_SLOTS_FULL, new ColorThemeEntry(Colors.LIGHT_RED));
