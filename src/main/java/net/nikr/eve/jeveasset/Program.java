35169795bf3333fb98aa8b3d51ee9e13372b0e16
GoldenGnu <GoldenGnu@users.noreply.github.com>
2022-05-31 12:28:04 +0200

    Release: Making ready for next release iteration 7.2.2

--- src/main/java/net/nikr/eve/jeveasset/Program.java
+++ src/main/java/net/nikr/eve/jeveasset/Program.java
@@ -117,7 +117,7 @@ public class Program implements ActionListener {
 		TIMER
 	}
 	//Major.Minor.Bugfix [Release Candidate n] [BETA n] [DEV BUILD #n];
-	public static final String PROGRAM_VERSION = "7.2.1";
+	public static final String PROGRAM_VERSION = "7.2.2 DEV BUILD 1";
 	public static final String PROGRAM_NAME = "jEveAssets";
 	public static final String PROGRAM_HOMEPAGE = "https://eve.nikr.net/jeveasset";
 	private static final boolean PROGRAM_DEV_BUILD = false;
