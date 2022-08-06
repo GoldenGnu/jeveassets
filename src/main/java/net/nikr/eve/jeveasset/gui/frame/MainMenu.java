48453bf2956864a9b2fc1f3217654ba5dbbca42a
GoldenGnu <GoldenGnu@users.noreply.github.com>
2022-08-06 16:29:51 +0200

    New Feature: Added mnemonic to the main menu

--- src/main/java/net/nikr/eve/jeveasset/gui/frame/MainMenu.java
+++ src/main/java/net/nikr/eve/jeveasset/gui/frame/MainMenu.java
@@ -23,6 +23,7 @@ package net.nikr.eve.jeveasset.gui.frame;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.concurrent.ExecutionException;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -82,6 +83,7 @@ public class MainMenu extends JMenuBar {
 
 //FILE
 		menu = new JMenu(GuiFrame.get().file());
+		menu.setMnemonic(KeyEvent.VK_F);
 		this.add(menu);
 
 		menuItem = new JMenuItem(GuiFrame.get().exit());
@@ -92,6 +94,7 @@ public class MainMenu extends JMenuBar {
 
 //TOOLS
 		menu = new JMenu(GuiFrame.get().tools());
+		menu.setMnemonic(KeyEvent.VK_T);
 		this.add(menu);
 
 		submenu = new JMenu(GuiFrame.get().netWorth());
@@ -220,6 +223,7 @@ public class MainMenu extends JMenuBar {
 
 //UPDATE
 		menu = new JMenu(GuiFrame.get().update());
+		menu.setMnemonic(KeyEvent.VK_U);
 		this.add(menu);
 
 		jUpdateMenu = new JMenuItem(GuiFrame.get().update1());
@@ -238,10 +242,12 @@ public class MainMenu extends JMenuBar {
 
 //TABLE
 		jTableMenu = new JMenu(GuiFrame.get().table());
+		jTableMenu.setMnemonic(KeyEvent.VK_A);
 		this.add(jTableMenu);
 
 //OPTIONS
 		menu = new JMenu(GuiFrame.get().options());
+		menu.setMnemonic(KeyEvent.VK_O);
 		this.add(menu);
 
 		menuItem = new JMenuItem(GuiFrame.get().accounts());
@@ -267,6 +273,7 @@ public class MainMenu extends JMenuBar {
 
 //HELP
 		menu = new JMenu(GuiFrame.get().help());
+		menu.setMnemonic(KeyEvent.VK_H);
 		this.add(menu);
 
 		menuItem = new JMenuItem(GuiFrame.get().linkWiki());
