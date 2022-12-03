72cdc3ba033f1707da2d711ecd97519e0598b138
GoldenGnu <GoldenGnu@users.noreply.github.com>
2022-12-03 10:55:11 +0100

    Bug Fix: Fixed auto update list file

--- tools/update/list.php
+++ tools/update/list.php
@@ -12,7 +12,7 @@ function dirToArray($dir, $fix = "", &$first = true) {
 			dirToArray($dir . DIRECTORY_SEPARATOR . $file, $fix . $file . "/", $first);
 		} elseif (!str_ends_with($file, ".php") 
 				&& !str_ends_with($file, ".md5")
-				&& !str_ends_with($file, ".dat")
+				&& (!str_ends_with($file, ".dat") || $file == "data.dat")
 				&& !str_starts_with($file, "installer")
 				) {
 			if ($first) {
@@ -24,16 +24,6 @@ function dirToArray($dir, $fix = "", &$first = true) {
 		}
 	}
 	closedir($handle);
-}
-
-function str_ends_with($haystack, $needle) {
-	// search forward starting from end minus needle length characters
-	return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
-}
-
-function str_starts_with($haystack, $needle) {
-    // search backwards starting from haystack length characters from the end
-    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
 }
 
 ?>
