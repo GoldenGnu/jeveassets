9199232bfc1a8fa9fa204ed515b98a21b947bcb7
GoldenGnu <GoldenGnu@users.noreply.github.com>
2023-03-15 10:39:17 +0100

    Code: Updated copyright to 2023

--- src/main/java/net/nikr/eve/jeveasset/gui/dialogs/AboutDialog.java
+++ src/main/java/net/nikr/eve/jeveasset/gui/dialogs/AboutDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2022 Contributors (see credits.txt)
+ * Copyright 2009-2023 Contributors (see credits.txt)
  *
  * This file is part of jEveAssets.
  *
@@ -57,7 +57,7 @@ public class AboutDialog extends JDialogCentered {
 
 		JEditorPane jProgram = createEditorPane(false,
 				"<div style=\"font-size: 30pt;\"><b>" + Program.PROGRAM_NAME + "</b></div>"
-				+ "Copyright &copy; 2009-2022 Contributors<br>"
+				+ "Copyright &copy; 2009-2023 Contributors<br>"
 				);
 
 		String s = Updater.getPackageMaintainers();
