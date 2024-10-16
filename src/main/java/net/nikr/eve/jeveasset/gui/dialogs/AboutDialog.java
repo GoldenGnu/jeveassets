16a7c40dac04d7391104a809378f31eed5566a7c
salartarium <34082385+salartarium@users.noreply.github.com>
2024-10-16 12:11:12 -0700

    Update AboutDialog.java
    add my name

--- src/main/java/net/nikr/eve/jeveasset/gui/dialogs/AboutDialog.java
+++ src/main/java/net/nikr/eve/jeveasset/gui/dialogs/AboutDialog.java
@@ -112,6 +112,7 @@ public class AboutDialog extends JDialogCentered {
 				+ "&nbsp;Lazaren<br>"
 				+ "&nbsp;Boran Lordsworth<br>"
 				+ "&nbsp;Ed Thelleres<br>"
+				+ "&nbsp;Salartarium<br>"
 				+ "<br>"
 				+ "<b>Retired Testers</b><br>"
 				+ "&nbsp;Varo Jan<br>"
