7d9a14fe74464b19bfc06d7db205c333e982e9cd
GoldenGnu <nkr@niklaskr.dk>
2025-04-27 14:44:33 +0200

    Release: Making ready for next release iteration 8.0.2

--- src/main/java/net/nikr/eve/jeveasset/Program.java
+++ src/main/java/net/nikr/eve/jeveasset/Program.java
@@ -130,7 +130,7 @@ public class Program implements ActionListener {
 		TIMER
 	}
 	//Major.Minor.Bugfix [Release Candidate n] [BETA n] [DEV BUILD #n];
-	public static final String PROGRAM_VERSION = "8.0.1";
+	public static final String PROGRAM_VERSION = "8.0.2 DEV BUILD 1";
 	public static final String PROGRAM_NAME = "jEveAssets";
 	public static final String PROGRAM_HOMEPAGE = "https://eve.nikr.net/jeveasset";
 	public static final String PROGRAM_USER_AGENT = PROGRAM_NAME + "/" + PROGRAM_VERSION.replace(" ", "_") + " (nkr@niklaskr.dk)";
