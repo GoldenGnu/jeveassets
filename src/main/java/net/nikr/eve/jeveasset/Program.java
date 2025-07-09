c570712ec03b4184300fb5db3a0e03a071536cdd
GoldenGnu <nkr@niklaskr.dk>
2025-07-09 13:15:42 +0200

    Code: Missing changes outbid global plex market

--- src/main/java/net/nikr/eve/jeveasset/Program.java
+++ src/main/java/net/nikr/eve/jeveasset/Program.java
@@ -751,7 +751,7 @@ public class Program implements ActionListener {
 			saveSettings("API Update");
 			Settings.waitForEmptySaveQueue();
 		}
-		profileManager.saveProfileSoft();
+		profileManager.saveProfile();
 	}
 
 	public synchronized void saveProfile() {
